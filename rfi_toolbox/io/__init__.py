594f6599ad1b92807d405fb5222c5722e9677f14
Preshanth Jagannathan <pjaganna@nrao.edu>
2025-12-31 10:53:20 -0700

    Updating to have msloader be imported without CASA for CI

--- rfi_toolbox/io/__init__.py
+++ rfi_toolbox/io/__init__.py
@@ -4,7 +4,13 @@ IO utilities for RFI toolbox.
 Handles CASA measurement set I/O and synthetic data injection.
 """
 
-from .ms_loader import MSLoader
+try:
+    from .ms_loader import MSLoader
+    _HAS_CASA = True
+except ImportError:
+    _HAS_CASA = False
+    MSLoader = None
+
 from .ms_injection import inject_synthetic_data
 
 __all__ = ["MSLoader", "inject_synthetic_data"]
